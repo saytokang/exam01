62af3e08f36fd4abad89de05db8e49c5fb9e0280
dony <we.binkan@gmail.com>
2018-04-08 22:03:46 +0900

    첫 파일 작성.

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MyTest {
+
+    @Test
+    public void test1() {
+        System.out.print("Hello");
+        assertTrue(true);
+
+    }
+}
