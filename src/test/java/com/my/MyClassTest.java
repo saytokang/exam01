2ee84717e7b0d2a097bf196dbe2df5d4bc9963e3
dony <we.binkan@gmail.com>
2018-04-08 22:20:06 +0900

    TDD 연습했음.

---
+++ src/test/java/com/my/MyClassTest.java
@@ -0,0 +1,14 @@
+package com.my;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyClassTest {
+
+    @Test
+    public void test1() {
+        System.out.print("한글로.");
+        assertTrue(true);
+    }
+}
