f956368d8ecdb8fcc0458075c72a9e8c50a8baec
kang don hee <saytokang@outlook.com>
2018-04-25 22:03:57 +0900

    Rebase 연습내용

--- src/test/java/com/my/MyClassTest.java
+++ src/test/java/com/my/MyClassTest.java
@@ -4,11 +4,31 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * 테스트 주석. 
+ * @author knowwheresoft
+ *
+ */
 public class MyClassTest {
 
     @Test
     public void test1() {
         System.out.print("한글로.");
+        System.out.print("한글로...");
+        System.out.print("한글로...");
+        System.out.print("한글로...");
+        System.out.print("한글로...");
+        System.out.print("한글로...");
         assertTrue(true);
     }
+    
+    @Test
+    public void test2() {
+        assertTrue(true);
+       System.out.println("1");
+       System.out.println("1");
+       System.out.println("1");
+    }
+    
+    
 }
---
+++ src/test/java/com/my/MyTest.java
@@ -0,0 +1,15 @@
+package com.my;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class MyTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
